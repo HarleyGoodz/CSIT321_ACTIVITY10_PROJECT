bb50cd4b53cb0f23f13d052e601c00788caac71c
Harley Cruz <johnharleycruz592@gmail.com>
2025-10-13 13:56:38 +0800

    project setup

---
+++ src/main/java/com/appdev/cruquihi/cruzquirantehisolerg6/Cruzquirantehisolerg6Application.java
@@ -0,0 +1,13 @@
+package com.appdev.cruquihi.cruzquirantehisolerg6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cruzquirantehisolerg6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cruzquirantehisolerg6Application.class, args);
+	}
+
+}
